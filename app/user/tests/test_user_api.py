fe2432a1301c86e5b3f622ded13a6f88aef9ad36
lianne <zhizhen.lianne.liu@gmail.com>
2025-03-30 15:58:13 +0100

    added tag model and implemented tag listing API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -129,7 +129,7 @@ class PrivateUserApiTests(TestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-    def test_retreive_profile_success(self):
+    def test_retrieve_profile_success(self):
         """Test retrieving profile for logged in user"""
         res = self.client.get(ME_URL)
 
